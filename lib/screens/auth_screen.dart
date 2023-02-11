cb6fb1b3f243a8e674360b5ca699202561f281d4
Annas Furquan Pasha <annasfurquan27@gmail.com>
2023-02-11 17:13:17 +0530

    authentication

--- lib/screens/auth_screen.dart
+++ lib/screens/auth_screen.dart
@@ -2,6 +2,7 @@ import 'dart:math';
 
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+
 import '../provider/auth.dart';
 import '../models/http_exception.dart';
 
@@ -30,7 +31,7 @@ class AuthScreen extends StatelessWidget {
                 ],
                 begin: Alignment.topLeft,
                 end: Alignment.bottomRight,
-                stops: [0, 1],
+                stops: const [0, 1],
               ),
             ),
           ),
@@ -94,7 +95,7 @@ class AuthCard extends StatefulWidget {
   _AuthCardState createState() => _AuthCardState();
 }
 
-class _AuthCardState extends State<AuthCard> {
+class _AuthCardState extends State<AuthCard>  with SingleTickerProviderStateMixin{
   final GlobalKey<FormState> _formKey = GlobalKey();
   AuthMode _authMode = AuthMode.Login;
   Map<String, String> _authData = {
@@ -103,6 +104,26 @@ class _AuthCardState extends State<AuthCard> {
   };
   var _isLoading = false;
   final _passwordController = TextEditingController();
+
+  late  AnimationController  _controller;
+  late Animation<Size> _heightAnimation;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    _controller  = AnimationController(vsync: this, duration: const Duration(milliseconds: 300));
+    _heightAnimation = Tween(begin: const Size(double.infinity , 260), end: const Size(double.infinity , 320))
+        .animate(CurvedAnimation(parent: _controller, curve: Curves.fastOutSlowIn));
+    // _heightAnimation.addListener(() => setState(() {}));
+  }
+
+  @override
+  void dispose() {
+    // TODO: implement dispose
+    super.dispose();
+    _controller.dispose();
+  }
 
   void _showErrorDialog(String message) {
     showDialog(context: context, builder: (ctx) => AlertDialog(
@@ -142,7 +163,7 @@ class _AuthCardState extends State<AuthCard> {
       var errorMessage = error.toString();
       _showErrorDialog(errorMessage);
     } catch(error) {
-      const errorMessage = 'could not authenticate you';
+      const errorMessage = 'Could not authenticate you';
       _showErrorDialog(errorMessage);
     }
       setState(() {
@@ -155,10 +176,12 @@ class _AuthCardState extends State<AuthCard> {
       setState(() {
         _authMode = AuthMode.Signup;
       });
+      _controller.forward();
     } else {
       setState(() {
         _authMode = AuthMode.Login;
       });
+      _controller.reverse();
     }
   }
 
@@ -170,13 +193,14 @@ class _AuthCardState extends State<AuthCard> {
         borderRadius: BorderRadius.circular(10.0),
       ),
       elevation: 8.0,
-      child: Container(
-        height: _authMode == AuthMode.Signup ? 320 : 260,
+      child: AnimatedBuilder(animation: _heightAnimation, builder: (ctx, ch) => Container(
+        // height: _authMode == AuthMode.Signup ? 320 : 260,
+        height: _heightAnimation.value.height,
         constraints:
-            BoxConstraints(minHeight: _authMode == AuthMode.Signup ? 320 : 260),
+        BoxConstraints(minHeight: _authMode == AuthMode.Signup ? 320 : 260),
         width: deviceSize.width * 0.75,
         padding: const EdgeInsets.all(16.0),
-        child: Form(
+        child: ch,)  , child: Form(
           key: _formKey,
           child: SingleChildScrollView(
             child: Column(
